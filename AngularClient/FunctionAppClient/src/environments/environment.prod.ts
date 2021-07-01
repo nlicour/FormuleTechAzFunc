12c6609317316d8cac364f038571338b03ca4f3a
Jeyaksan RAJARATNAM <jeyaksan.rajaratnam@neos-sdi.com>
2021-07-01 17:15:42 +0200

    Fix build issue

--- AngularClient/FunctionAppClient/src/environments/environment.prod.ts
+++ AngularClient/FunctionAppClient/src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: false,
+  baseApiUrl: "https://formuletech-azurefonction-demo.azurewebsites.net/api",
+  setLedRegbKey: "UlOaNh1Jz8JxzcyUcYB9lw06qmB5pPYvwmzrHQXyDm9oxxriaNDeHA==",
+  setLedStateFunctionKey: "1QUdlpIGSFNxz24kdMVPXFDm3Mw0aY7nT9hv8lNRchw0fwNm/a/SXw==",
 };
