1cd40cbd59ec3b6450cb6b3b17366e08fb9eeaad
Jeyaksan RAJARATNAM <jeyaksan.rajaratnam@neos-sdi.com>
2021-07-01 17:49:08 +0200

    fix build

--- AngularClient/FunctionAppClient/src/app/app.component.ts
+++ AngularClient/FunctionAppClient/src/app/app.component.ts
@@ -4,7 +4,7 @@ import { LedControllerService } from './led-controller.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.css']
 })
 
 export class AppComponent {
